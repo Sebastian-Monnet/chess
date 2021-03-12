243ef1bde024bb0dee67bf374c13151e1443c9a8
Sebastian Monnet <sebastian.monnet@gmail.com>
2021-03-12 16:49:22 +0000

    Added iterative minimax

--- main.py
+++ main.py
@@ -1,51 +1,113 @@
 import display
 import pygame, sys, chess
+import copy
 import random
 import random_agent
 import player_agent
+import minimax_agent
+print('a')
+import matplotlib.pyplot as plt
+print('b')
+import datetime
 import time
+import numpy as np
 from pygame.locals import *
 
 board = chess.Board()
 
 pygame.init()
 
-screen = pygame.display.set_mode((display.BOARD_LENGTH + 2 * display.BORDER,
-                                  display.BOARD_LENGTH + 2 * display.BORDER), 0, 32)
+
 
 def to_play(board):
     return board.fen().split(' ')[1]
 
-def play_game(screen, agent_w, agent_b, pov):
+def play_game(agent_w, agent_b, pov, show=True):
     board = chess.Board()
-    display.draw_board(screen)
+    if show:
+        screen = pygame.display.set_mode((display.BOARD_LENGTH + 2 * display.BORDER,
+                                          display.BOARD_LENGTH + 2 * display.BORDER), 0, 32)
+        display.draw_board(screen)
+
+    new_time = datetime.datetime.now()
+    white_time_arr = []
+    black_time_arr = []
+    san_arr = []
+    white_move_arr = []
+    black_move_arr = []
 
     while not board.is_game_over():
         if not board.is_game_over():
             turn = to_play(board)
+
             if turn=='w':
+                temp_board = copy.deepcopy(board)
                 move = agent_w.get_move(board, turn)
+                white_move_arr.append(str(move))
+                old_time, new_time = new_time, datetime.datetime.now()
+                white_time_arr.append((new_time - old_time).total_seconds())
+                #print('white time: ', white_time_arr[-1])
             else:
                 move = agent_b.get_move(board, turn)
+                old_time, new_time = new_time, datetime.datetime.now()
+                black_time_arr.append((new_time - old_time).total_seconds())
+                black_move_arr.append(str(move))
+                san_arr.append(temp_board.variation_san([chess.Move.from_uci(m) for m in [white_move_arr[-1],
+                                                                                      black_move_arr[-1]]]))
+                #print(san_arr[-1])
+
+                #print('black time: ', black_time_arr[-1])
             board.push_uci(move)
 
-            print(board)
+            if show:
+                display.draw_board(screen)
+                display.draw_all_pieces(board.fen(), screen, pov)
+                pygame.display.update()
 
-
-            display.draw_board(screen)
-            display.draw_all_pieces(board.fen(), screen, pov)
+        if show:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
             pygame.display.update()
 
-        for event in pygame.event.get():
-            if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
-        pygame.display.update()
+    if show:
+        running = True
+        while running:
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    running = False
 
-        time.sleep(0.2)
 
-    return board.result()
+    return board.result(), white_time_arr, black_time_arr, san_arr
 
-result = play_game(screen, player_agent.PlayerAgent(), random_agent.RandomAgent(), pov='w')
-#result = play_game(screen, random_agent.RandomAgent(), random_agent.RandomAgent(), pov='w')
-print(result)
+#result = play_game(screen, player_agent.PlayerAgent(), random_agent.RandomAgent(), pov='w')
+
+games = 1
+white_arr = - np.ones((games, 300))
+black_arr = - np.ones((games, 300))
+
+for i in range(games):
+    result, white_times, black_times, san_arr = play_game(minimax_agent.MinimaxAgentIter(1),
+                                                  minimax_agent.MinimaxAgentRecur(0), pov='w', show=False)
+    white_arr[i][:len(white_times)] = white_times
+    black_arr[i][:len(black_times)] = black_times
+
+print(white_arr)
+white_arr = np.array(white_arr)
+black_arr = np.array(black_arr)
+
+to_plot_white = np.zeros(100)
+to_plot_black = np.zeros(100)
+for i in range(100):
+    to_plot_white[i] = np.mean(white_arr[:, i][white_arr[:,i] != -1])
+    to_plot_black[i] = np.mean(black_arr[:, i][black_arr[:, i] != -1])
+
+
+#print(result)
+plt.plot(to_plot_white, label='white')
+plt.plot(to_plot_black, label='black')
+plt.legend()
+plt.show()
--- minimax_agent.py
+++ minimax_agent.py
@@ -1,22 +1,163 @@
 import chess
 import copy
 import random
+import datetime
 
 PIECE_VALUES = {'P' : 1, 'R' : 5, 'N' : 3.2, 'B' : 3.3, 'Q' : 9, 'K' : 900}
 
-class MinimaxAgent:
+class MinimaxAgentIter:
     def __init__(self, depth):
         self.depth = depth
 
     def get_move(self, board, turn):
-        move_arr = MinimaxAgent.get_legal_moves(board)
+        move_arr = MinimaxAgentIter.minimax_iteration(board, self.depth)
+        return str(random.choice(move_arr))
+
+    @staticmethod
+    def evaluate(board, true_player):
+        if board.is_game_over():
+            result = board.result()
+            if result == '1/2-1/2':
+                return 0
+            if result == '0-1':
+                winner = 'b'
+            elif result == '1-0':
+                winner = 'w'
+            if winner == true_player:
+                return 10000
+            else:
+                return -10000
+
+        row_arr = board.fen().split('/')
+        row_arr[-1] = row_arr[-1].split(' ')[0]
+
+        # value from white's perspective
+        val = 0
+        for row in row_arr:
+            for char in row:
+                if not char.isalpha():
+                    continue
+                abs_val = PIECE_VALUES[char.upper()]
+                if char.isupper():
+                    val += abs_val
+                else:
+                    val -= abs_val
+
+        if true_player == 'w':
+            return val
+        else:
+            return - val
+    @staticmethod
+    def get_legal_moves(board):
+        legal_arr = [move for move in board.legal_moves]
+        return legal_arr
+
+    @staticmethod
+    def get_next_states(board, moves):
+        true_fen = board.fen()
+        state_arr = []
+        for move in moves:
+            board.set_fen(true_fen)
+            board.push(move)
+            state_arr.append(board.fen())
+        return state_arr
+
+    @staticmethod
+    def minimax_iteration(board, max_depth):
+        child_dic, parent_dic, val_dic, moves_dic = {}, {}, {}, {}
+
+        cur_board = copy.deepcopy(board)
+
+        cur_depth = 0
+        root = board.fen()
+        cur_node = root
+        true_player = cur_node.split(' ')[1]
+        add_children = True
+
+        num_iters = 0
+        d = datetime.datetime.now()
+        while root not in val_dic:
+
+            most_recent_lag = (datetime.datetime.now() - d).total_seconds()
+            d = datetime.datetime.now()
+            num_iters += 1
+            if cur_depth == max_depth:
+                val_dic[cur_node] = MinimaxAgentIter.evaluate(cur_board, true_player)
+                cur_depth -= 1
+                add_children = False
+                if max_depth != 0:
+                    cur_node = parent_dic[cur_node]
+                    cur_board = chess.Board(cur_node)
+                most_recent_lag = (datetime.datetime.now() - d).total_seconds()
+                continue
+
+            if add_children:
+                moves = MinimaxAgentIter.get_legal_moves(cur_board)
+                child_arr = MinimaxAgentIter.get_next_states(cur_board,moves)
+                child_dic[cur_node] = child_arr
+                for child in child_arr:
+                    parent_dic[child] = cur_node
+                add_children = False
+                most_recent_lag = (datetime.datetime.now() - d).total_seconds()
+
+            has_all_children = True
+            for child in child_dic[cur_node]:
+                if child not in val_dic:
+                    cur_node = child
+                    cur_board = chess.Board(cur_node)
+                    cur_depth += 1
+                    if child not in child_dic:
+                        add_children = True
+                    has_all_children = False
+                    break
+            most_recent_lag = (datetime.datetime.now() - d).total_seconds()
+
+            if has_all_children:
+                child_values = [val_dic[child] for child in child_dic[cur_node]]
+                if child_values == []:
+                    val_dic[cur_node] = MinimaxAgentIter.evaluate(cur_board, true_player)
+                elif cur_depth % 2 == 0:
+                    val_dic[cur_node] = max(child_values)
+                else:
+                    val_dic[cur_node] = min(child_values)
+                if cur_depth > 0:
+                    cur_node = parent_dic[cur_node]
+                    cur_depth -= 1
+                cur_board.set_fen(cur_node)
+                most_recent_lag = (datetime.datetime.now() - d).total_seconds()
+
+        b = datetime.datetime.now()
+
+        moves = MinimaxAgentIter.get_legal_moves(board)
+        best_move_arr = []
+        best_val = -999999
+        for move in moves:
+            cur_board = chess.Board(cur_node)
+            cur_board.push(move)
+            if val_dic[cur_board.fen()] == best_val:
+                best_move_arr.append(move)
+            elif val_dic[cur_board.fen()] > best_val:
+                best_val = val_dic[cur_board.fen()]
+                best_move_arr = [move]
+        c = datetime.datetime.now()
+
+
+        return best_move_arr
+
+
+class MinimaxAgentRecur:
+    def __init__(self, depth):
+        self.depth = depth
+
+    def get_move(self, board, turn):
+        move_arr = MinimaxAgentRecur.get_legal_moves(board)
         random.shuffle(move_arr)
         best_val = -10000
         best_ind = 0
         for i in range(len(move_arr)):
             new_board = copy.deepcopy(board)
             new_board.push(move_arr[i])
-            new_val = MinimaxAgent.minimax(new_board, turn, self.depth)
+            new_val = MinimaxAgentRecur.minimax(new_board, turn, self.depth)
             if new_val > best_val:
                 best_val = new_val
                 best_ind = i
@@ -65,7 +206,7 @@ class MinimaxAgent:
     @staticmethod
     def minimax(board, true_player, depth):
         if depth == 0 or board.is_game_over():
-            return MinimaxAgent.evaluate(board, true_player)
+            return MinimaxAgentRecur.evaluate(board, true_player)
 
         to_play = board.fen().split(' ')[1]
         if to_play == true_player:
@@ -75,29 +216,31 @@ class MinimaxAgent:
 
         if is_maximise:
             eval = -10000
-            for move in MinimaxAgent.get_legal_moves(board):
+            for move in MinimaxAgentRecur.get_legal_moves(board):
                 new_board = copy.deepcopy(board)
                 new_board.push(move)
 
-                new_val = MinimaxAgent.minimax(new_board, true_player, depth - 1)
+                new_val = MinimaxAgentRecur.minimax(new_board, true_player, depth - 1)
                 eval = max(eval, new_val)
 
             return eval
 
         else:
             eval = 10000
-            for move in MinimaxAgent.get_legal_moves(board):
+            for move in MinimaxAgentRecur.get_legal_moves(board):
                 new_board = copy.deepcopy(board)
                 new_board.push(move)
-                new_val = MinimaxAgent.minimax(new_board, true_player, depth - 1)
+                new_val = MinimaxAgentRecur.minimax(new_board, true_player, depth - 1)
 
                 eval = min(eval, new_val)
             return eval
+
+
 
 
 #board = chess.Board(fen='7k/5B2/8/8/8/4K3/8/p7 b - - 0 31')
 #board = chess.Board(fen='6k1/5r2/8/8/8/3BK3/p7/8 w - - 0 30')
 
 #print(board)
-#print(MinimaxAgent.minimax(board, 'w', 4))
-#print(MinimaxAgent.evaluate(board, 'w'))
+#print(MinimaxAgentIter.minimax(board, 'w', 4))
+#print(MinimaxAgentIter.evaluate(board, 'w'))
